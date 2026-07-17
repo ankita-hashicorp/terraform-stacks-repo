fd350d2d26d8185ca46240948d1736aca19de1f5
Ankita Doshi <ankita.doshi@hashicorp.com>
2026-07-17 18:27:23 +0530

    adding more random resources

--- modules/stacks-tfpolicy-module/main.tf
+++ modules/stacks-tfpolicy-module/main.tf
@@ -18,3 +18,20 @@ resource "random_id" "random_id_25" {
         instance_type = "t3.micro"
     }
 }
+
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "random_shuffle" "az" {
+  input        = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  result_count = 1
+}
+
+resource "random_pet" "custom_name" {
+  length    = 3
+  prefix    = "prod"
+  separator = "_"
+}
